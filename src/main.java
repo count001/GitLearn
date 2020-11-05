1ee8c47848c4916ce45249b04ad0c2f55674bc96
Yamyyy <2396649622@qq.com>
2020-11-05 20:25:30 +0800

    [ADD]增加了for循环输出

--- src/main.java
+++ src/main.java
@@ -2,5 +2,8 @@ public class main {
 
     public static void main(String[] args){
         System.out.println("hello world!!!");
+
+
+        for (int i=0;i<10;i++) System.out.println("hello");
     }
 }
